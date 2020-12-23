e74546dfffb7e3aa5dcf1c8c0f8ca6c29d4a05a8
Krishan Patel <krishan711@gmail.com>
2020-12-23 16:42:23 +0000

    .

--- scripts/common/js.webpack.js
+++ scripts/common/js.webpack.js
@@ -6,6 +6,7 @@ const defaultParams = {
 module.exports = (inputParams = {}) => {
   const params = {...defaultParams, ...inputParams};
   const babelConfig = buildBabelConfig(params);
+  console.log('babelConfig', JSON.stringify(babelConfig));
   return {
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.jsx'],
