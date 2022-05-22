95590aa29a9120332f9b3ae788fda77cfc3b0df8
Krishan Patel <krishan711@gmail.com>
2022-05-22 12:01:26 +0100

    .

---
+++ scripts/util.js
@@ -0,0 +1,14 @@
+
+const removeUndefinedProperties = (obj) => {
+  return Object.keys(obj).reduce((current, key) => {
+    if (obj[key] !== undefined) {
+      // eslint-disable-next-line no-param-reassign
+      current[key] = obj[key];
+    }
+    return current;
+  }, {});
+};
+
+module.exports = {
+  removeUndefinedProperties,
+};
