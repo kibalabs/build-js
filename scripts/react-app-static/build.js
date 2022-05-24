fdd587778681593b08125be6640868c5123cd842
Krishan Patel <krishan711@gmail.com>
2022-05-24 15:40:55 +0100

    Fix: correct imports in react-app-static (#67)
    * .
    
    * .

--- scripts/react-app-static/build.js
+++ scripts/react-app-static/build.js
@@ -9,8 +9,8 @@ const makeCssWebpackConfig = require('../common/css.webpack');
 const makeImagesWebpackConfig = require('../common/images.webpack');
 const makeJsWebpackConfig = require('../common/js.webpack');
 const { createAndRunCompiler } = require('../common/webpackUtil');
+const makeModuleWebpackConfig = require('../module/module.webpack');
 const makeReactAppWebpackConfig = require('../react-app/app.webpack');
-const makeReactComponentWebpackConfig = require('../react-component/component.webpack');
 const { removeUndefinedProperties } = require('../util');
 const { renderHtml } = require('./static');
 
@@ -56,7 +56,7 @@ module.exports = (inputParams = {}) => {
     makeJsWebpackConfig({ ...params, polyfill: false, react: true }),
     makeImagesWebpackConfig(params),
     makeCssWebpackConfig(params),
-    makeReactComponentWebpackConfig({ ...params, entryFilePath: appEntryFilePath, outputDirectory: buildDirectoryPath, excludeAllNodeModules: true }),
+    makeModuleWebpackConfig({ ...params, entryFilePath: appEntryFilePath, outputDirectory: buildDirectoryPath, excludeAllNodeModules: true }),
   );
   if (params.webpackConfigModifier) {
     nodeWebpackConfig = params.webpackConfigModifier(nodeWebpackConfig);
