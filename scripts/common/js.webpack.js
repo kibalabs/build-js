b483541869f169ec90dbdc1742cbbc9424a10a64
Krishan Patel <krishan711@gmail.com>
2023-05-25 12:15:02 +0100

    Fiddle: update dependencies (#89)

--- scripts/common/js.webpack.js
+++ scripts/common/js.webpack.js
@@ -25,8 +25,7 @@ const buildJsWebpackConfig = (inputParams = {}) => {
           test: /\.(j|t)sx?$/,
           // NOTE(krishan711): node_modules should possibly be included so that they are polyfilled by core-js
           // NOTE(krishan711): if this is un-commented, include core-js as an entry in react-app webpack config
-          // NOTE(krishan711): this stupid is-plain-obj is used by react-markdown in @kibalabs/ui-react. Find a way to specify it somewhere else (or compile all node modules!)
-          // exclude: /(node_modules\/(?!(is-plain-obj))|build|dist)\//,
+          // exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
