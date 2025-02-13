296152f865ea7a088841d4b54ea2e80a6bd9400c
Krishan Patel <krishan711@gmail.com>
2025-02-13 15:44:29 +0000

    .

--- scripts/module-rolldown/generateTypeDeclarationsPlugin.js
+++ scripts/module-rolldown/generateTypeDeclarationsPlugin.js
@@ -1,4 +1,3 @@
-import path from 'node:path';
 
 import { generateTypescriptDeclarations } from '../typing/generateDeclarations.js';
 import { buildTsConfig } from '../typing/ts.config.js';
@@ -17,7 +16,6 @@ export const generateTypeDeclarationsPlugin = ({
         // NOTE(krishan711): below works to output one file but it
         // incorrectly puts declare module "index" instead of
         // declare module "<package_name>"
-        
         // outFile: path.join(outputDirectory, 'index.d.ts'),
       });
     },
