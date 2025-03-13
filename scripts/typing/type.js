cca10d32bc2b4ff9a9d5cd20d95c17647affb479
Krishan Patel <krishan711@gmail.com>
2025-03-13 15:22:49 +0000

    Fix: await buildParams for typing (#112)

--- scripts/typing/type.js
+++ scripts/typing/type.js
@@ -16,7 +16,7 @@ const defaultParams = {
 };
 
 export const runTyping = async (inputParams = {}) => {
-  const params = buildParams(defaultParams, inputParams);
+  const params = await buildParams(defaultParams, inputParams);
   const tsConfig = buildTsConfig(params);
   // NOTE(krishan711): I couldn't find a way to filter node_modules in the glob (filtering needed for lerna repos)
   const files = glob.sync(path.join(params.directory || './src', '**', '*.{ts,tsx}'));
